b6fac75c5e770e81f913b6c86f7165374e51055e
adarsh2023calc <adarshs2023@gmail.com>
2025-06-06 06:28:21 +0530

    Added gemini and Dockerfile

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from fastapi.middleware.cors import CORSMiddleware
 import httpx
 import os
 from dotenv import load_dotenv
+import json
+import requests
 import math
 
 load_dotenv()
@@ -60,3 +62,38 @@ async def fetch_place_info(lat: float = Query(...), lng: float = Query(...)):
     except:
          return httpx.RequestError("Failure! Unable to extract data")
 
+
+@app.get("/get_gemini_suggest")
+
+
+async def get_gemini_suggest(items: str= Query(...)):
+    prompt = f"""
+    Here is a data:
+    {json.dumps(items, indent=2)}
+
+    From the data, gather insights and display them clearly.
+    """
+
+    
+
+    GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+    url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key={GEMINI_API_KEY}"
+
+    headers = {
+        "Content-Type": "application/json"
+    }
+
+    data = {
+        "contents": [
+            {
+                "parts": [{"text": prompt}]
+            }
+        ]
+    }
+
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+    result = response.json()
+    print(result)
+    output = result.get("candidates", [{}])[0].get("content", {}).get("parts", [{}])[0].get("text", "No output received")
+
+    return {"output": output}
